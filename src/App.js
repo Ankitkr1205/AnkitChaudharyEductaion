40083cf01d0164605b242062f781a4734816e4d4
Ankit Chaudhary <chaudharyankit573nets@gmail.com>
2023-03-16 17:02:02 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,31 @@ import "./App.css";
 
 function App() {
   return (
+    // <div>
+    //   <form>
+    //     <label>Username</label>
+    //     <input type="text" />
+    //     <label>Password</label>
+    //     <input type="Password" />
+    //     <button
+    //       onClick={() => {
+    //         alert('i am clicked')
+    //       }}
+    //     >
+    //       Submit
+    //     </button>
+    //   </form>
+    // </div>
     <div>
-      <h1>GITHUB</h1>
+      <form>
+        <label className="abc">First name</label>
+        <input type="text" id="fname" name="fname" value="John" />
+        <br />
+        <label>Last name</label>
+        <input type="text" id="lname" name="lname" value="Doe" />
+        <br />
+        <button style={{ color: "red" }}>SUBMIT</button>
+      </form>
     </div>
   );
 }
